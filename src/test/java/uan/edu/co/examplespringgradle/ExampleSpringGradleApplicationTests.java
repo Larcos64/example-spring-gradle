9af66c5b0edbed17785d6e778e57c70c6b0f8d16
Larcos665 <latombe8@misena.edu.co>
2022-03-20 21:49:33 -0500

    First commit

---
+++ src/test/java/uan/edu/co/examplespringgradle/ExampleSpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package uan.edu.co.examplespringgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
